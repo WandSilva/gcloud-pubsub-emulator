ce47c7cde2c274e66cb94f8a7c78ba1279ec4e61
Wanderson Silva <wanderson.silva@lend.tech>
2022-09-03 16:53:37 -0300

    create publisher utils to topic management

---
+++ pubsub-utils/publisher.py
@@ -0,0 +1,66 @@
+
+import argparse
+
+from google.cloud import pubsub_v1
+
+
+def create_topic(project_id: str, topic_id: str) -> None:
+    """Create a new PubSub topic."""
+
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
+    topic = publisher.create_topic(request={"name": topic_path})
+
+    print(f"Created topic: {topic.name}")
+
+
+def list_topics(project_id: str) -> None:
+    """Lists all Pub/Sub topics in the given project."""
+
+    publisher = pubsub_v1.PublisherClient()
+    project_path = f"projects/{project_id}"
+
+    for topic in publisher.list_topics(request={"project": project_path}):
+        print(topic)
+
+
+def delete_topic(project_id: str, topic_id: str) -> None:
+    """Deletes an existing Pub/Sub topic."""
+
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
+
+    publisher.delete_topic(request={"topic": topic_path})
+
+    print(f"Topic deleted: {topic_path}")
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+
+    parser.add_argument("project_id", help="Your Google Cloud project ID")
+    subparsers = parser.add_subparsers(dest="command")
+    subparsers.add_parser("list", help=list_topics.__doc__)
+
+    create_parser = subparsers.add_parser("create", help=create_topic.__doc__)
+    create_parser.add_argument("topic_ids", nargs="+")
+
+    delete_parser = subparsers.add_parser("delete", help=delete_topic.__doc__)
+    delete_parser.add_argument("topic_ids", nargs="+")
+    
+    args = parser.parse_args()
+
+    if args.command == "list":
+        list_topics(args.project_id)
+    
+    elif args.command == "create":
+        for topic_id in args.topic_ids:
+            create_topic(args.project_id, topic_id)
+    
+    elif args.command == "delete":
+        for topic_id in args.topic_ids:
+            delete_topic(args.project_id, topic_id)
+
